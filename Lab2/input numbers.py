23b22406fc14e4a1a3d59c546033ec467b4020c5
Khaled Jalal Al-arashi <k.j.alarashi@gmail.com>
2025-12-01 20:55:36 +0300

    Update input numbers.py

--- Lab2/input numbers.py
+++ Lab2/input numbers.py
@@ -3,8 +3,8 @@ num2 = int(input("Enter Second Number : "))
 sum = num1+num2
 print(type(sum))
 
-# , ---> any datatype - any datatype
+# , ---> any datatype , any datatype
 print("Your Result is : ",sum)
 
-# concatenation ---> str - str
+# concatenation ---> str + str
 print("Your Result is : "+str(sum))
